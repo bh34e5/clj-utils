6a8c3308c5962f0a9a453096518ba1bf1871edf9
bh34e5 <b.hester0614@yahoo.com>
2023-10-22 17:08:31 -0400

    Convert utils to lein project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-utils "0.1.0-SNAPSHOT"
+  :description "Utilities compiled into a single repository"
+  :url "https://github.com/bh34e5/clj-utils"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns clj-utils.core})

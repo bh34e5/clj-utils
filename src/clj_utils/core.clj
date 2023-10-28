fc3d4ff37b196374d5a4c1b7ed6e746d96764eda
bh34e5 <b.hester0614@yahoo.com>
2023-10-27 22:02:13 -0400

    adds features
    - if-else
    - ensure-seq
    - noisy-let
    - noisy-clamp
    
    also fixes ecase

--- src/clj_utils/core.clj
+++ src/clj_utils/core.clj
@@ -17,9 +17,46 @@
   `(defs-base def ~@parts))
 
 (defmacro ecase [e & cases]
-  `(case ~e
-     ~@cases
-     (throw (ex-info "Unmatched option in case"))))
+  `(let [e# ~e]
+     (case e#
+       ~@cases
+      (throw (ex-info "Unmatched option in case" {:option e#})))))
+
+(defmacro if-else [if-form else-form]
+  `(let [v# ~if-form]
+     (if v# v# ~else-form)))
+
+(defn ensure-seq [in]
+  (if (seq? in)
+    in
+    (cons in nil)))
+
+(defn- vec-unpair
+  [pairs]
+  (loop [cur (vector) ;; vector, so that conj puts things at the back
+         pairs pairs]
+    (let [cur-pair (first pairs)
+          f (conj cur (first cur-pair))]
+      (if cur-pair
+        (recur (conj f (second cur-pair))
+               (rest pairs))
+        cur))))
+
+(defmacro noisy-let
+  [[& bindings] & forms]
+  (let [new-bindings (for [b (partition 2 bindings)]
+                       `((~(first b) ~(second b))
+                         (nop# (println '~(first b) ~(first b)))))
+        unpaired (vec-unpair new-bindings)
+        extra-unpaired (vec-unpair unpaired)]
+    `(let ~extra-unpaired
+       ~@forms)))
+
+(defn noisy-clamp
+  [n mn mx]
+  (let [mn-clp (if (< n mn) (do (println "Num less than " mn "!!") mn) n)
+        mx-clp (if (> n mx) (do (println "Num greater than " mx "!!") mx) n)]
+    mx-clp))
 
 (defmacro import-static
   "Imports the named static fields and/or static methods of the class
